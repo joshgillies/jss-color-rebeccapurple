551094b9ef01d50defa78ae37e3f438019c60ac9
Josh Gillies <github@joshgilli.es>
2016-01-07 21:50:13 +1100

    Remove babel-tape-runner in favor of tape with require hooks

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ export default function jssRebeccapurple () {
   return rule => {
     if (rule.type !== 'regular') return
 
-    for (var prop in rule.style) {
+    for (let prop in rule.style) {
       if (rule.style[prop].indexOf('rebeccapurple') !== -1) {
         rule.style[prop] = rebeccapurple(rule.style[prop])
       }
